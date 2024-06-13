ae0747ffc4f0872537e35415e51ae78ef7ff1a71
thimnan <168484095+thimnan@users.noreply.github.com>
2024-06-13 15:55:38 +0000

    Update MainActivity3.kt
    accidentally uploaded the wrong copy on github but didn't know to upload a new app on the same so I edited the old one's codes

--- app/src/main/java/com/example/practicum/MainActivity3.kt
+++ app/src/main/java/com/example/practicum/MainActivity3.kt
@@ -7,6 +7,34 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    
+ val days = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+        val maxTemps = arrayOf(30, 32, 28, 29, 31, 33, 34)
+        val minTemps = arrayOf(20, 22, 19, 21, 23, 24, 25)
+         val conditions = arrayOf("Sunny", "Cloudy", "Rainy", "Sunny", "Partly Cloudy", "Sunny", "Rainy")
+
+
+
+
+            setContentView(R.layout.activity_main3)
+
+            val textView = findViewById<TextView>(R.id.textView)
+
+            val dataString = buildString {
+                append("Days: ${days.joinToString(", ")}\n")
+                append("Max Temperatures: ${maxTemps.joinToString(", ")}\n")
+                append("Min Temperatures: ${minTemps.joinToString(", ")}\n")
+                append("Conditions: ${conditions.joinToString(", ")}")
+            }
+
+            textView.text = dataString
+
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+        buttonBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
 
